3a24076da046f3103ae0973d38786da3709aca42
Mandella-thi <thiagogarc@gmail.com>
2022-02-03 15:43:47 -0300

    interface de rede

--- helloworld.tf
+++ helloworld.tf
@@ -22,14 +22,14 @@ resource "aws_vpc" "vpc_brq" {
     Name = "VPC_legal2"
   }
 }
-#resource "aws_subnet" "subrede_brq" {
- # vpc_id     = aws_vpc.vpc_brq.id
-  #cidr_block = "10.0.1.0/24"
+resource "aws_subnet" "subrede_brq" {
+  vpc_id     = aws_vpc.vpc_brq.id
+  cidr_block = "10.0.1.0/24"
 
-  #tags = {
-   # Name = "subrede_testebrq"
-  #}
-#}
+  tags = {
+    Name = "subrede_testebrq"
+  }
+}
 resource "aws_internet_gateway" "gw_brq" {
   vpc_id = aws_vpc.vpc_brq.id
 
@@ -52,5 +52,60 @@ resource "aws_route_table" "example" {
 
   tags = {
     Name = "Testeteste123"
+  }
+}
+#resource "aws_route_table_association" "associacao" {
+ # subnet_id      =  aws_subnet.subrede_brq.id
+  #route_table_id = example.rotas_brq.id
+#}
+resource "aws_security_group" "firewall" {
+  name        = "abrir portas"
+  description = "Abrir porta 22(SSH, 443(HTTPS))e 80HTTP"
+  vpc_id      = aws_vpc.vpc_brq.id
+
+  ingress {
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "Coisinha"
+  }
+}
+resource "aws_network_interface" "test" {
+  subnet_id       = aws_subnet.public_a.id
+  private_ips     = ["10.0.0.50"]
+  security_groups = [aws_security_group.web.id]
+
+  attachment {
+    instance     = aws_instance.test.id
+    device_index = 1
   }
 }
